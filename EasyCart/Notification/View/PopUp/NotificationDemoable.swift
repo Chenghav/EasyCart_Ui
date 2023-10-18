6c30161342fba7b81f8eec115e891dda1ba12a41
SalinChhun <salinchhun123079@gmail.com>
2023-10-18 19:07:08 +0700

    today

---
+++ EasyCart/Notification/View/PopUp/NotificationDemoable.swift
@@ -0,0 +1,59 @@
+//
+//  Demoable.swift
+//  EasyCart
+//
+//  Created by PVH_002 on 18/10/23.
+//
+
+import Foundation
+import UIKit
+import FittedSheets
+
+protocol NotificationDemoable {
+
+}
+
+// MARK:  - PopUp FittedSheets -
+extension NotificationDemoable {
+    
+    static func addSheetEventLogging(to sheet: SheetViewController) {
+        let previousDidDismiss = sheet.didDismiss
+        sheet.didDismiss = {
+            print("did dismiss")
+            previousDidDismiss?($0)
+        }
+        
+        let previousShouldDismiss = sheet.shouldDismiss
+        sheet.shouldDismiss = {
+            print("should dismiss")
+            return previousShouldDismiss?($0) ?? true
+        }
+        
+        let previousSizeChanged = sheet.sizeChanged
+        sheet.sizeChanged = { sheet, size, height in
+            print("Changed to \(size) with a height of \(height)")
+            previousSizeChanged?(sheet , size, height)
+        }
+    }
+    static func openDemo(from parent: UIViewController, in view: UIView?,btnName : String?, titleLab1: String?) {
+        let useInlineMode = view != nil
+        
+        let controller = UIStoryboard(name: "NotificationSB", bundle: nil).instantiateViewController(withIdentifier: "NotificationPopUpViewController") as! NotificationPopUpViewController
+//         controller.titleButton = btnName!
+//         controller.titleLab = titleLab1!
+        let options = SheetOptions(
+            shouldExtendBackground: true,
+            useFullScreenMode: true,
+            shrinkPresentingViewController: true,
+            useInlineMode: useInlineMode
+        )
+        
+        let sheetController = SheetViewController(controller: controller, options: options)
+        sheetController.allowPullingPastMaxHeight = false
+        sheetController.setSizes([.fixed(240)])
+        
+        
+        parent.present(sheetController, animated: true, completion: nil)
+    }
+}
+
---
+++ EasyCart/Notification/View/PopUp/NotificationPopUpViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NotificationPopUpViewController.swift
+//  EasyCart
+//
+//  Created by PVH_002 on 18/10/23.
+//
+
+import UIKit
+
+class NotificationPopUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
