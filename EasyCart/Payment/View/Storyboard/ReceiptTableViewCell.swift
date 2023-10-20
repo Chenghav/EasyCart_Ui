4b89310adaa63112fba47cf75d5f6a98adca76c5
ToTo1403 <ditasuhito@gmail.com>
2023-10-20 16:55:38 +0700

    myloveforyou

---
+++ EasyCart/Payment/View/Storyboard/ReceiptTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ReceiptTableViewCell.swift
+//  EasyCart
+//
+//  Created by ksga on 18/10/23.
+//
+
+import UIKit
+
+class ReceiptTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ EasyCart/Payment/View/Storyboard/RecieptController.swift
@@ -0,0 +1,8 @@
+//
+//  RecieptController.swift
+//  EasyCart
+//
+//  Created by ksga on 18/10/23.
+//
+
+import Foundation
